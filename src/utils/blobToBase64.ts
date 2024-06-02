3dbd19d808be396ff05468ca12cad464066ecc89
junezhuo <yzzhuo2@gmail.com>
2024-06-02 15:45:17 +0300

    fix: build type error

--- src/utils/blobToBase64.ts
+++ src/utils/blobToBase64.ts
@@ -1,7 +1,7 @@
 const blobToBase64 = (blob: Blob, callback: any) => {
   const reader = new FileReader();
   reader.onload = function () {
-    const base64data = reader?.result?.split(",")[1];
+    const base64data = typeof reader.result === 'string' ? reader.result.split(",")[1] : '';
     callback(base64data);
   };
   reader.readAsDataURL(blob);
