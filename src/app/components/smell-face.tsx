a7190efe2853dffc55e1098917f6d1f44494cc25
junezhuo <yzzhuo2@gmail.com>
2024-06-03 13:38:14 +0300

    style: reduce blur of circle UI

--- src/app/components/smell-face.tsx
+++ src/app/components/smell-face.tsx
@@ -3,7 +3,7 @@ export default function SmileyFace({
 }) {
     return (
         <div className="flex flex-col items-center justify-center h-screen relative p-36">
-           <div className={`animate-${loading ? 'spin' : 'pulse'} absolute w-64 h-64 md:w-80 md:h-80 bg-gradient-to-r from-sky-300 to-indigo-300 rounded-full blur-xl`}></div>
+           <div className={`animate-${loading ? 'spin' : 'pulse'} absolute w-64 h-64 md:w-80 md:h-80 bg-gradient-to-r from-sky-300 to-indigo-300 rounded-full blur-lg`}></div>
             {/* <div className="flex justify-center items-center space-x-2">
                 <div className="bg-gradient-to-r from-sky-300 to-indigo-300 rounded-full blur-md w-16 h-36 animate-bounce"></div>
                 <div className="bg-gradient-to-r from-sky-300 to-indigo-300 rounded-full blur-md w-16 h-36 animate-bounce200"></div>
